7784d2f131012d4a63df8d6fe6acc2fa1c0740c5
MollyWhisper <nastya79218634824@gmail.com>
2026-04-08 15:38:49 +0300

    Create webhook.js

---
+++ api/webhook.js
@@ -0,0 +1,11 @@
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    const data = req.body;
+
+    console.log('Tilda data:', data);
+
+    res.status(200).json({ status: 'ok' });
+  } else {
+    res.status(405).json({ error: 'Method not allowed' });
+  }
+}
